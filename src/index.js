16e22852c8de03aec177d4fbae19c8c2a7caa7d8
victoria <victoria-butrym@tut.by>
2019-03-24 18:55:10 +0300

    feat: add function that returns true if the sequence is correct and false otherwise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let br_c = bracketsConfig;
+  let array = str.split('');
+
+  for (let i = 0; i < array.length; i++){
+    for(let j = 0; j < br_c.length; j++){
+      if (array[i] === br_c[j][0] && array[i+1] === br_c[j][1]){
+        array.splice(i, 2);
+        if (array.length == 0){
+          return true;
+        }
+        i=-1;
+      }
+    }
+  }
+  return false;
 }
